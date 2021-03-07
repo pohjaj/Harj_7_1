a7d89e62a3eaed788672c191bb7c85a92a64d6f6
DESKTOP-UD1HI3H\jarip <jari.pohja@elisanet.fi>
2021-03-07 10:59:45 +0200

    Harjoitus 7.1

---
+++ app/src/main/java/com/example/harj_7_1/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.harj_7_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view){
+        String message = "Hello world!";
+        System.out.println(message);
+
+    }
+}
